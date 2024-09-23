0ccf0f708408b7a9ce111271ce2530ff185af3bf
Rashmika Dillhara <rashmikadil2023@gmail.com>
2024-09-23 21:40:36 +0530

    Add files via upload

---
+++ StyleSphere/script.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 9000);
+
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("slide");
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
